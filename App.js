678c4d0572ba2f08385cd4fd953fd9c99645b774
OtabekVaxobov <elot2017@mail.ru>
2021-02-12 19:51:49 +0300

    /image-picker

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import React from 'react';
-import { Image,  StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import logo from './assets/logo.png'; 
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  let [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={logo} style={{width:305, height: 159}} />
+      <Image
+        source={{ uri: 'https://i.imgur.com/TkIrScD.png' }}
+        style={styles.logo}
+      />
       <Text style={styles.instructions}>
-      styled text
+        To share a photo from your phone with a friend, just press the button
+        below!
       </Text>
-      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} 
-      style={styles.logo} />
-      <TouchableOpacity 
-        onPress={() => alert('Hello')} style={ styles.button}>
-        <Text style={ styles.buttonText}>Pick a photo</Text>
 
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
     </View>
   );
@@ -30,20 +60,26 @@ const styles = StyleSheet.create({
   logo: {
     width: 305,
     height: 159,
-    marginBottom: 10,
+    marginBottom: 20,
   },
-  instructions:{
+  instructions: {
     color: '#888',
     fontSize: 18,
     marginHorizontal: 15,
+    marginBottom: 10,
   },
   button: {
-    backgroundColor: "blue",
+    backgroundColor: 'blue',
     padding: 20,
     borderRadius: 5,
   },
   buttonText: {
     fontSize: 20,
     color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
   },
 });
